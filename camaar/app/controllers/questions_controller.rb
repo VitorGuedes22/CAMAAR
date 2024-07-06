9a5c4cb44e80da1e9e93e6a11104e5cdd1f6352c
Marcelo Vitor <marcelovitor890@gmail.com>
2024-07-05 22:28:34 -0300

    Documentação e refatoração questions_controller

--- camaar/app/controllers/questions_controller.rb
+++ camaar/app/controllers/questions_controller.rb
@@ -1,10 +1,48 @@
+# == QuestionsController
+#
+# Este controlador é responsável por gerenciar as ações relacionadas à model Question.
+#
+# === Filtros
+# - protect_from_forgery with: :null_session
+#
+# === Ações Públicas
+# - home
+# - create
+#
+# === Métodos Privados
+# - question_params
+
 class QuestionsController < ApplicationController
   protect_from_forgery with: :null_session
 
+  # GET /questions/home
+  #
+  # Retorna todas as instâncias da model Question.
+  #
+  # ==== Exemplo de resposta
+  #
+  #   [
+  #     { id: 1, title: 'Question 1', question_type: 'Type 1', ... },
+  #     { id: 2, title: 'Question 2', question_type: 'Type 2', ... }
+  #   ]
+  #
   def home
     @questions = Question.all
   end
 
+  # POST /questions
+  #
+  # Cria uma nova instância da model Question com os parâmetros fornecidos.
+  #
+  # ==== Parâmetros
+  #
+  # - +question_params+ - Parâmetros permitidos para a criação da model Question.
+  #
+  # ==== Exemplo de resposta
+  #
+  # - JSON: Retorna a nova instância criada com status :created.
+  # - JSON: Retorna erros de validação com status :unprocessable_entity.
+  #
   def create
     question = Question.new(question_params)
     if question.save
@@ -16,6 +54,16 @@ class QuestionsController < ApplicationController
 
   private
 
+  # Permite uma lista de parâmetros confiáveis para a criação da model Question.
+  #
+  # ==== Parâmetros permitidos
+  #
+  # - +:title+
+  # - +:question_type+
+  # - +options_attributes+: [
+  #     - +:text+
+  #   ]
+  #
   def question_params
     params.require(:question).permit(:title, :question_type, options_attributes: [:text])
   end
