ef0cf41c4e89e6466c450d7f474ca6b0981c5eed
Marcelo Vitor <marcelovitor890@gmail.com>
2024-07-05 22:32:06 -0300

    Documentação e refatoraçã de surveys_controller

--- camaar/app/controllers/surveys_controller.rb
+++ camaar/app/controllers/surveys_controller.rb
@@ -1,6 +1,30 @@
+# == SurveysController
+#
+# Este controlador é responsável por gerenciar as ações relacionadas à model Survey.
+#
+# === Filtros
+# - protect_from_forgery with: :null_session
+#
+# === Ações Públicas
+# - create
+#
+# === Métodos Privados
+# - survey_params
+
 class SurveysController < ApplicationController
   protect_from_forgery with: :null_session
 
+  # POST /surveys
+  #
+  # Cria uma nova instância da model Survey com os parâmetros fornecidos.
+  #
+  # ==== Parâmetros
+  # - +params[:survey]+ - Parâmetros permitidos para a criação da model Survey, incluindo perguntas e opções.
+  #
+  # ==== Exemplo de resposta
+  # - JSON: Retorna a nova instância criada com status :created.
+  # - JSON: Retorna erros de validação com status :unprocessable_entity.
+  #
   def create
     survey = Survey.new(survey_params)
 
@@ -13,6 +37,19 @@ class SurveysController < ApplicationController
 
   private
 
+  # Permite uma lista de parâmetros confiáveis para a criação da model Survey.
+  #
+  # ==== Parâmetros permitidos
+  #
+  # - +:title+
+  # - +questions_attributes+: [
+  #     - +:title+
+  #     - +:question_type+
+  #     - +options_attributes+: [
+  #         - +:text+
+  #       ]
+  #   ]
+  #
   def survey_params
     params.require(:survey).permit(:title, questions_attributes: [:title, :question_type, options_attributes: [:text]])
   end
